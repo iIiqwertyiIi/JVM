980e130e43523531aa4e8f7bc8055978ba56424c
VOLP1 <eduvolpi5@gmail.com>
2025-06-08 20:57:58 -0300

    mudanca no attributes

--- attribute_info.h
+++ attribute_info.h
@@ -1,7 +1,10 @@
 #ifndef ATTRIBUTE_INFO_H
 #define ATTRIBUTE_INFO_H
+
 #include "types.h"
 
-attribute_info * read_attribute_info();
+attribute_info* read_attribute_info(cp_info** constant_pool);
 
-#endif
+void free_attribute_info(attribute_info* attribute, cp_info** constant_pool);
+
+#endif 
