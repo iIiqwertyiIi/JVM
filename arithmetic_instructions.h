ba664d1abc5343accd8b765c0e168c443b528d26
TiagoBuson <tiagobuson@gmail.com>
2025-07-02 21:43:04 -0300

    instruções aritméticas: sub

--- arithmetic_instructions.h
+++ arithmetic_instructions.h
@@ -9,6 +9,10 @@ int iadd(Frame * frame);
 int ladd(Frame * frame);
 int fadd(Frame * frame);
 int dadd(Frame * frame);
+int isub(Frame * frame);
+int lsub(Frame * frame);
+int fsub(Frame * frame);
+int dsub(Frame * frame);
 
 
 
