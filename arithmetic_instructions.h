afe800d33b4ae3c493c104f85d86a4d0ace9d767
TiagoBuson <tiagobuson@gmail.com>
2025-07-02 21:39:06 -0300

    instruções aritméticas: add

---
+++ arithmetic_instructions.h
@@ -0,0 +1,16 @@
+#ifndef ARITHMETIC_H
+#define ARITHMETIC_H
+
+#include <math.h>
+#include "types.h"
+#include "helper.h"
+
+int iadd(Frame * frame);
+int ladd(Frame * frame);
+int fadd(Frame * frame);
+int dadd(Frame * frame);
+
+
+
+
+#endif // ARITHMETIC_H
