5106b1909b71f0b8f80a4d1f608c17d4e2c971a1
VOLP1 <eduvolpi5@gmail.com>
2025-07-04 12:23:00 -0300

    feat: implementa funcoes de conversao de tipo para instrucoes JVM e manipulacao de pilha

--- types.h
+++ types.h
@@ -159,4 +159,33 @@ typedef struct FrameStack {
     Frame * top_frame;
 } FrameStack;
 
+// Estrutura para instruções
+typedef struct Instruction {
+    u1 opcode;
+    u1 * operands;
+    u2 operand_count;
+} Instruction;
+
+// Funções de manipulação da pilha de operandos
+u4 remove_from_stack(Frame * frame);
+void add_to_stack(Frame * frame, u4 value);
+u4 peek_stack(Frame * frame);
+
+// Funções de conversão de tipo para instruções da JVM
+int d2f(Frame * frame, Instruction instruction);
+int d2i(Frame * frame, Instruction instruction);
+int d2l(Frame * frame, Instruction instruction);
+int f2d(Frame * frame, Instruction instruction);
+int f2i(Frame * frame, Instruction instruction);
+int f2l(Frame * frame, Instruction instruction);
+int i2b(Frame * frame, Instruction instruction);
+int i2c(Frame * frame, Instruction instruction);
+int i2d(Frame * frame, Instruction instruction);
+int i2f(Frame * frame, Instruction instruction);
+int i2l(Frame * frame, Instruction instruction);
+int i2s(Frame * frame, Instruction instruction);
+int l2d(Frame * frame, Instruction instruction);
+int l2f(Frame * frame, Instruction instruction);
+int l2i(Frame * frame, Instruction instruction);
+
 #endif
