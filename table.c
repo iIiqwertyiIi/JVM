b6627cfeddb57a4696691115dafde39c86236ea9
TiagoBuson <tiagobuson@gmail.com>
2025-07-11 11:03:14 -0300

    adicionando função table switch

---
+++ table.c
@@ -0,0 +1,31 @@
+#include "table.h"
+
+int table_switch(Frame * frame, Instruction instruction) {
+  const u4 start = frame->pc.position - 1;
+  const u1 padding = 3 - (start % 4);
+  for (int i = 0; i < padding; i++) {
+    u1read_buffer(&frame->pc);
+  }
+
+  int32_t default_offset = u4_to_int(u4read_buffer(&frame->pc));
+  int32_t low = u4_to_int(u4read_buffer(&frame->pc));
+  int32_t high = u4_to_int(u4read_buffer(&frame->pc));
+  int32_t index = u4_to_int(remove_from_stack(frame));
+
+  TSwtich table_switch = {
+    .size = high - low + 1,
+    .offsets = malloc(sizeof(u4) * (high - low + 1)),
+  };
+
+  for (int i = low; i <= high; i++) {
+    table_switch.offsets[i - low] = u4_to_int(u4read_buffer(&frame->pc));
+  }
+
+  if (index >= low && index <= high) {
+    frame->pc.position = start + table_switch.offsets[index - low];
+  } else {
+    frame->pc.position = start + default_offset;
+  }
+
+  return 0;
+}
---
+++ table.h
@@ -0,0 +1,10 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "types.h"
+#include "instructions.h"
+#include "reader.h"
+
+int table_switch(Frame * frame, Instruction instruction);
+
+#endif // TABLE_H
